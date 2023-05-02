f8a6631cb559729e7e84032fb4727f221ab9fad3
The Xorus <jdpepi@gmail.com>
2023-05-02 23:04:07 +0200

    Add Dockerfile, docker-compose.yml, and .dockerignore files for containerization.

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.11
+
+RUN pip install poetry==1.4.2 && \
+    poetry config virtualenvs.create false
+
+#RUN apt update && apt install -y <PACKAGE>\ <Package>
+#RUN apt-get clean && \ rm -rf /var/lib/apt/lists/*
+
+COPY . /app
+
+WORKDIR /app
+ENV PYTHONPATH=${PYTHONPATH}:${PWD}
+
+RUN poetry lock && poetry install --only main
+
+EXPOSE 8000
+
+ENTRYPOINT ["/app/start.sh", "prod"]
--- start.sh
+++ start.sh
@@ -6,11 +6,12 @@ cd "$(dirname "$0")"
 if [ "$1" = "prod" ]; then
     echo "Starting Uvicorn server in production mode..."
     # we also use a single worker in production mode so socket.io connections are always handled by the same worker
-    uvicorn main:app --workers 1 --log-level info --port 80
+#    uvicorn app.main:app --workers 1 --log-level info --port 8000
+    uvicorn app.main:app --log-level info --port 8000 --host 0.0.0.0
 elif [ "$1" = "dev" ]; then
     echo "Starting Uvicorn server in development mode..."
     # reload implies workers = 1
-    uvicorn main:app --reload --log-level debug --port 8000
+    uvicorn app.main:app --reload --log-level debug --port 8000
 else
     echo "Invalid parameter. Use 'prod' or 'dev'."
     exit 1
